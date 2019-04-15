144f4d1c48f8b75862077aa5cfef8e9d4f08f1ae
sanas\sana <srinivasveerasana@gmail.com>
2019-04-15 15:00:27 -0400

    commitjenkins

---
+++ jenkinsjava/src/test/java/TestJenkins/JenkinsDriver.java
@@ -0,0 +1,18 @@
+package TestJenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class JenkinsDriver {
+	
+	@Test
+	public static void facebook(){
+		System. setProperty("webdriver.chrome.driver", "C:\\Users\\sana\\Downloads\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+
+	}
+
+}
