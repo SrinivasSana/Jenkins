c6afb51a50ceeb6b693fd90b6fd9f82328295ac8
SrinivasSana <srinivasveerasana@gmail.com>
2019-04-19 05:58:57 -0400

    Update JenkinsDriver.java

--- jenkinsjava/src/test/java/TestJenkins/JenkinsDriver.java
+++ jenkinsjava/src/test/java/TestJenkins/JenkinsDriver.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 public class JenkinsDriver {
 	
 	@Test
-	public static void facebook1(){
+	public static void facebook12(){
 		System. setProperty("webdriver.chrome.driver", "C:\\Users\\sana\\Downloads\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.facebook.com/");
